d970d774aaf7213a9ecdeebc55ad08246cff32c7
dar96 <100532984+dar96@users.noreply.github.com>
2022-03-28 16:37:02 +0200

    Update RutaProtegida.jsx

--- src/layouts/RutaProtegida.jsx
+++ src/layouts/RutaProtegida.jsx
@@ -1,7 +1,7 @@
 import { Outlet, Navigate } from "react-router-dom"
 import useAuth from "../hooks/useAuth"
 import Header from "../components/Header"
-import Sidebar from "../components/SideBar"
+import Sidebar from "../components/Sidebar"
 
 const RutaProtegida = () => {
 
