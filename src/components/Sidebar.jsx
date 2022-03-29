4f32ebe3806cd2bb1a53a34c1ae93ab680db8d63
dar96 <100532984+dar96@users.noreply.github.com>
2022-03-29 20:48:26 +0200

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,6 @@ const Sidebar = () => {
 
   return (
     <aside className="md:w-1/3 lg:w-1/5 xl:w-1/6 px-5 py-10">
-        <p className="text-xl font-bold">Hola</p>
 
         <Link
            to="crear-proyecto" 
